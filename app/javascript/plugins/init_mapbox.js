ddd15fd955ce8d9d6575d11d17bb3c90a1fd7bb4
Alon Shvartsman <alonalexandershvartsman@gmail.com>
2019-12-02 11:14:28 +0200

    added default zoom view to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,11 +2,14 @@ import mapboxgl from 'mapbox-gl';
 
 const mapElement = document.getElementById('map');
 
-const buildMap = () => {
+const buildMap = (longitude, latitude) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [34.7743034, 32.0779315],
+    zoom: 12.5,
+    attributionControl: false
   });
 };
 
@@ -14,17 +17,18 @@ const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // add this
-      .addTo(map);
-  });
-};
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) // add this
+          .addTo(map)
+      });
+    };
+
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1500 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 1500 });
 };
 
 const initMapbox = () => {
