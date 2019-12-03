e3dfc9c8494bd63800e193338e926258b832be62
Ofir Elboym <ofirfir26@gmail.com>
2019-12-03 15:04:57 +0200

    my profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   def dashboard
     if current_user
       @reports = current_user.reports
-      @events = current_user.events
-      authorize @events
-      authorize @reports
+      @events = current_user.attendances
+      # authorize @events
+      # authorize @reports
     else
       flash[:alert] = "You are not authorized to perform this action."
       redirect root_path
