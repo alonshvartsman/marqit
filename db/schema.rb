91a393d30b6ce4015501640312b7efb6bda5f410
Ofir Elboym <ofirfir26@gmail.com>
2019-12-04 10:42:02 +0200

    mapbox be gone

--- db/schema.rb
+++ db/schema.rb
@@ -53,9 +53,9 @@ ActiveRecord::Schema.define(version: 2019_12_01_125724) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
@@ -67,9 +67,9 @@ ActiveRecord::Schema.define(version: 2019_12_01_125724) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.string "first_name"
     t.string "last_name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
