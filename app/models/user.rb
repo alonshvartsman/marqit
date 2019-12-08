193670950368821717a92417bf1fa43e7e3ecc10
Alon Shvartsman <alonalexandershvartsman@gmail.com>
2019-12-08 17:28:04 +0200

    created avatar sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   acts_as_voter
-  after_create :default_picture
+  # after_create :default_picture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,13 +12,10 @@ class User < ApplicationRecord
   has_many :messeged_events, through: :messages, source: :events
   mount_uploader :photo, PhotoUploader
 
-  def default_picture
-    raise
-    if self.photo
-      self.photo = params(:user[:photo])
-    else
-      self.photo = File.open(File.join(".", "app/assets/images/profile_green.svg"))
-      self.save
-    end
-  end
+  # def default_picture
+  #   unless self.photo
+  #     current_user.photo = File.open(File.join(".", "app/assets/images/logo.png"))
+  #     current_user.save
+  #   end
+  # end
 end
