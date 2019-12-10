f902add29cab45a3f630adc5a6aac7d29f690c37
Ofir Elboym <ofirfir26@gmail.com>
2019-12-10 14:43:12 +0200

    seeding

--- db/schema.rb
+++ db/schema.rb
@@ -59,9 +59,9 @@ ActiveRecord::Schema.define(version: 2019_12_09_153249) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(version: 2019_12_09_153249) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.string "first_name"
     t.string "last_name"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
